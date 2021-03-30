7f39ddca347b17c3024b4981d7e35c2987b91c59
AliciaYien <ayien1@my.bcit.ca>
2021-03-30 16:02:45 -0700

    card with button

---
+++ my-app/pages/index.js
@@ -0,0 +1,17 @@
+function TextImageBoxUI(){
+
+return <div class="main" style={{
+  width:300,
+  height:400,
+}}>
+  <h3>Default Text</h3>
+
+  <img src="http://placekitten.com/250/150"></img>
+
+  <p><span>M</span>inions ipsum jeje tulaliloo baboiii tatata bala tu butt daa bappleees belloo! Jeje ti aamoo! Po kass para tú bee do bee do bee do. Poulet tikka masala tulaliloo para ti bananaaaa daa hana dul sae potatoooo.   </p>
+
+  <button>Read More</button>
+</div>
+}
+
+export default TextImageBoxUI;
