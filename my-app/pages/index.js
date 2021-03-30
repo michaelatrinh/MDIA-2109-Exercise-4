546a436e2ee6785ff22138210d676c93bdf13a93
ariellecastro <caastrox@gmail.com>
2021-03-30 15:54:03 -0700

    Created a log-in page component

---
+++ my-app/pages/index.js
@@ -0,0 +1,25 @@
+function ContactPage(){
+
+  return <div class="body">
+    
+    <div class="container">
+
+    <h3 class="login">LOGIN</h3>
+
+    <p>Email</p>
+    <input class="input-1"></input>
+
+    <p>Password</p>
+    <input class="input-2"></input>
+
+    <br></br><br></br>
+    <button class="submit">LOGIN</button>
+
+    </div>
+    
+    </div>
+
+}
+
+
+export default ContactPage;
