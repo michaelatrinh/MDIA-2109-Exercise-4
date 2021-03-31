26800e26372f48743a5a6a2788d19e7b81ca19b1
Michael <mtrinh8@my.bcit.ca>
2021-03-30 17:20:51 -0700

    completed navigation side bar component

---
+++ my-app/pages/index.js
@@ -0,0 +1,18 @@
+
+
+function navigationBar(){
+    return <div id="navBar">
+
+        <ul className="navBarUL">
+            <h3><span className="navBarTitle">Title</span></h3>
+            <li><a href=''>Page1</a></li>
+            <li><a href=''>Page2</a></li>
+            <li><a href=''>Page3</a></li>
+            <li><a href=''>Page4</a></li>
+            <li className="navLiButton"><button className="navLiButton2">Exit</button></li>
+        </ul>
+
+    </div>
+}
+
+export default navigationBar;
